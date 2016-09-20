39b176cf3c92f98695a546396619f0ae457b905b
anniekbh <anniekbh@gmail.com>
2016-09-20 11:22:37 -0500

    actualizando archivos

--- js/app.js
+++ js/app.js
@@ -1,16 +1,35 @@
 window.addEventListener("load", function() {
 	var boton = document.getElementById("boton");
-	boton.addEventListener("click", function() {
-		var crearMensaje = document.getElementById("mensaje").value;
+	var crearMensaje = document.getElementById("mensaje");
 
-	    var newMensaje = document.createElement("div");
+	boton.addEventListener("click", function(e) {
+		e.preventDefault();
+		mostrarMensaje(crearMensaje.value);
+		crearMensaje.value = "";
+	});
+
+	function mostrarMensaje(crearMensaje){
+		var newMensaje = document.createElement("div");
 	    var mostrarMensaje = document.createTextNode(crearMensaje);
 	    newMensaje.appendChild(mostrarMensaje);
-
 	    var placeText = document.getElementById("hola");
 	    placeText.insertBefore(newMensaje, placeText.childNodes[0]);
-	    document.getElementById("mensaje").value = "";
+	    };
+
+	crearMensaje.addEventListener("keyup", function(){
+		var limiteMaximo = 140;
+		var caracteresIngresados = this.value.length;
+		var contar = document.getElementById("contador");
+		contar.innerText = limiteMaximo - caracteresIngresados;
+		if (caracteresIngresados>=limiteMaximo){
+			contar.classList.add("colorRed");
+			boton.disabled = true;
+		} else {
+			contar.classList.remove("colorRed");
+			boton.disabled=false;
+		}
 	});
+	
 });
 
 
